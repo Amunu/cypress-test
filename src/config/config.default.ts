2ed91a2714d9d2cafbfa468bac9f3b08fb89821f
yilin.fyl <yilin.fyl@alibaba-inc.com>
2022-08-01 10:41:06 +0800

    init

---
+++ src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1654830935863_3960',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
